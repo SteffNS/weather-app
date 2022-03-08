8182a9c7da5b94324a3a91bd0ee090609292df92
Steffen Neves-Silva <87664485+SteffNS@users.noreply.github.com>
2022-03-08 15:31:18 -0800

    update api info
    updated api key out into a .env, updated .gitignore

--- src/components/Condition.js
+++ src/components/Condition.js
@@ -1,14 +1,19 @@
 const Condition = (props) => {
     return(
         <div>
+            {/* Error prompt if invalid city */}
             {props.error && <p>Error, please enter a valid city. Check your spelling.</p>}
-            {props.loading && <div>Loading</div>}
+            {/* Loading while fetching info from api */}
+            {props.loading && <p>Loading</p>}
+
             {props.responseObj.cod === 200 ? 
+                //if valid, outputs the info 
                 <div>
                     <p>{props.responseObj.name}</p>
                     <p>Currently it is {Math.round(props.responseObj.main.temp)} degrees with {props.responseObj.weather[0].description}.</p>
                 </div>
             : 
+                //if invalid, outputs nothing
                 null 
             }
         </div>
