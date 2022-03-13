1a91dede31b185363bf371ec5c1946dd823db157
Steffen Neves-Silva <87664485+SteffNS@users.noreply.github.com>
2022-03-13 16:29:35 -0700

    updated condition.js
    updated condition js to add country and start work on sunset/sunrise

--- src/components/Condition.js
+++ src/components/Condition.js
@@ -1,7 +1,10 @@
+//imports
+import { useState } from "react";
+
 const Condition = (props) => {
+    
     return(
         <div>
-
             {/* Error prompt if invalid city */}
             {props.error && <p>Error, please enter a valid city. Check your spelling.</p>}
             {/* Loading while fetching info from api */}
@@ -10,11 +13,12 @@ const Condition = (props) => {
             {props.responseObj.cod === 200 ? 
                 //if valid, outputs the info 
                 <div>
-                    <p>{props.responseObj.name} {props.responseObj.main.country}</p>
+                    <p>{props.responseObj.name}, {props.responseObj.sys.country}</p>
                     <p>Currently it is {Math.round(props.responseObj.main.temp)} degrees with {props.responseObj.weather[0].description}.</p>
                     <p>
                         Right now it feels like {Math.round(props.responseObj.main.feels_like)}&#176;, highs will be around {Math.round(props.responseObj.main.temp_max)}&#176; and lows around {Math.round(props.responseObj.main.temp_min)}&#176;
                     </p>
+                    <p>Sunrise is at {props.responseObj.sys.sunset} and sunset is at {props.responseObj.sys.sunset}</p>
                 </div>
             : 
                 //if invalid, outputs nothing
